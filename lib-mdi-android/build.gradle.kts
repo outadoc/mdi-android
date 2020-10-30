6e756c2b4cb40e572561c920a384229836c44e11
Baptiste Candellier <outadoc@gmail.com>
2020-10-30 20:09:32 +0100

    Fix some stuff

--- lib-mdi-android/build.gradle.kts
+++ lib-mdi-android/build.gradle.kts
@@ -16,6 +16,7 @@
 
 plugins {
     id("com.android.library")
+    id("maven-publish")
     kotlin("android")
 }
 
@@ -48,6 +49,19 @@ kotlin {
 dependencies {
     implementation(project(":lib-mdi-common"))
     implementation("androidx.appcompat:appcompat:1.3.0-alpha02")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            setUrl("https://maven.pkg.github.com/outadoc/mdi-android")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 val scriptDirectory = "${rootProject.projectDir}/script"
