c182a92b6253445c81432ca1a0e6a576be6a51a3
Baptiste Candellier <outadoc@gmail.com>
2020-11-08 20:32:49 +0100

    Create custom TextView

---
+++ lib-mdi-android/src/main/java/fr/outadoc/mdi/MdiFontIconView.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 Baptiste Candellier
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package fr.outadoc.mdi
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.*
+
+class MdiFontIconView : AppCompatTextView {
+
+    constructor(context: Context) : super(context, null)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(
+        context,
+        attrs,
+        android.R.attr.textViewStyle
+    )
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    init {
+        setTextAppearance(R.style.TextAppearance_MaterialDesignIcons)
+    }
+}
